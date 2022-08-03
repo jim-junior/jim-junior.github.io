fe2b2c30da1e12f6d24330f5a125ebaaca0a0ede
jim-junior <jim-junior@users.noreply.github.com>
2022-08-03 15:10:34 +0000

    deploy: a0f3ffa82dd3c5ca88c59bc0c5dd63e4d2412b8c

---
+++ _next/static/rxsvpJtOyJdbyO4gA7wHE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-12c33cf8a9b8c142.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
