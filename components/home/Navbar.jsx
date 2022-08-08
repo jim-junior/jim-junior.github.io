f3f84a4cc20f1fc02f71052cb815c240e1cbe123
Beingana Jim Junior <jimjunior854@gmail.com>
2022-08-08 15:09:02 +0000

    Add theree jj

---
+++ components/home/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <h2></h2>
+        </div>
+        <div></div>
+    </nav>
+  )
+}
+
+export default Navbar
