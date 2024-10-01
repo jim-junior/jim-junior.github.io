1cc9613d23acc188afff1a87c6c02b756d376fdb
jim-junior <jimjunior854@gmail.com>
2024-10-01 19:15:45 +0300

    Add latex support

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import mdx from "@mdx-js/rollup";
 import rehypeMdxTitle from "rehype-mdx-title";
 import rehypePrettyCode from "rehype-pretty-code";
 import moonlightTheme from './assets/moonlight-ii.json' with { type: 'json' };
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,8 +13,11 @@ export default defineConfig({
     {
       enforce: "pre",
       ...mdx({
-        remarkPlugins: [],
-        rehypePlugins: [ [
+        remarkPlugins: [
+          remarkMath
+        ],
+        rehypePlugins: [
+          rehypeKatex, [
           rehypePrettyCode,
           {
             keepBackground: false,
