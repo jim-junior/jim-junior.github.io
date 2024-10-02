994b73ac261501115e1a63ce3c041dd6e8ca4904
jim-junior <jimjunior854@gmail.com>
2024-10-02 12:24:36 +0300

    Add post title

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,10 +1,14 @@
 import Navbar from "../components/navbar";
 import { Container } from "@mui/joy";
 import { Post } from "../components/blog/post-data";
+import { Helmet } from "react-helmet-async";
 
 const BlogPostPage = ({ post }: { post: Post }) => {
   return (
     <>
+      <Helmet>
+        <title>{post.title}</title>
+      </Helmet>
       <Navbar />
       <Container>{post.content}</Container>
     </>
