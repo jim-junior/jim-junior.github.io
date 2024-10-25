3d620a1f3a8d7a565ff10b1e96cce43583bb6972
jim-junior <jimjunior854@gmail.com>
2024-10-25 16:02:09 +0300

    remove kubernetes operator blog temprarily

--- src/components/blog/post-data.tsx
+++ src/components/blog/post-data.tsx
@@ -1,6 +1,6 @@
 import { COMPONENTS } from "../mdx-conponents";
 //import CraneOperatorPost from "./blogs/simplifiying-kubernetes.md";
-import CraneOperatorSpecPost from "./blogs/crane-operator-spec.md";
+//import CraneOperatorSpecPost from "./blogs/crane-operator-spec.md";
 import JavaScriptProxies from "./blogs/javascript-proxies-a-beginners-guide.md";
 //import OrbitonJSBlog from "./blogs/3-minutes-to-orbiton-js-the-ultimate-javascript-ui-library.md";
 import ReactLibrary from "./blogs/how-to-create-an-npm-library-from-react-components.md";
@@ -19,7 +19,7 @@ export const posts = [
       "https://images.pexels.com/photos/1181354/pexels-photo-1181354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     cartegory: "CLOUD NATIVE",
     content: <CraneOperatorPost components={COMPONENTS} />,
-  }, */
+  },
   {
     title: "Building a Kubernetes Operator. A Beginner's Guide",
     description:
@@ -30,7 +30,7 @@ export const posts = [
       "https://www.epsglobal.com/Media-Library/EPSGlobal/Blog/kubernets2.jpg",
     cartegory: "CLOUD NATIVE",
     content: <CraneOperatorSpecPost components={COMPONENTS} />,
-  },
+  }, */
   {
     title: "How to create a responsive custom video player in React",
     description:
