cd33c2559fab9be49db1a76c04e767dbd52131be
jim-junior <jimjunior854@gmail.com>
2025-01-02 13:48:08 +0300

    rm v

--- src/components/homepage/header.tsx
+++ src/components/homepage/header.tsx
@@ -61,9 +61,6 @@ const Header = () => {
           technologies in the fields of Cloud Computing, DevOps, and Web
           Development and Distributed Systems. */}
         </Typography>
-        <Typography level="body-lg">
-          Currently venturing into Artificial Intelligence
-        </Typography>
         <Box
           sx={{
             display: "flex",
